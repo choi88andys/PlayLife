fff7a8840b93d81881a7e2ce2ee45d7ea7084dc4
ChoiYS <choi88andys@gmail.com>
2021-12-21 13:14:23 +0900

    Initial Commit

---
+++ PlayLife/PlayLifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayLifeApp.swift
+//  PlayLife
+//
+//  Created by MacAndys on 2021/12/21.
+//
+
+import SwiftUI
+
+@main
+struct PlayLifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
