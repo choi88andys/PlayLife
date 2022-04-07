0984ae07c3b6e94b15bbb8b0d9461e3264c1595f
ChoiYS <choi88andys@gmail.com>
2022-04-07 16:20:22 +0900

    UI improved

--- PlayLife/View/Common/StatusView.swift
+++ PlayLife/View/Common/StatusView.swift
@@ -38,7 +38,7 @@ struct StatusView: View {
 
 
 struct VerticalStatusView: View {
-    var spacing: Double = SettingConstants.fontSize*0.5
+    var spacing: Double = SettingConstants.fontSize*0.2
     let imageView: Image
     let color: Color
     let value: String
@@ -54,7 +54,8 @@ struct VerticalStatusView: View {
             Text(value)
                 .font(.system(size: SettingConstants.fontSize))
         }
-        .frame(width: SettingConstants.fontSize*7, height: SettingConstants.fontSize*7)
+        .frame(width: SettingConstants.isEnglish ? SettingConstants.fontSize*7 : SettingConstants.fontSize*4.5,
+               height: SettingConstants.isEnglish ? SettingConstants.fontSize*7 : SettingConstants.fontSize*5.5)
         .overlay(
             RoundedRectangle(cornerRadius: SettingConstants.fontSize*1.3)
                 .stroke(Color.gray, lineWidth: 1.8)
